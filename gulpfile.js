b6079f21970ca1668768c3224afc8b0f2e21c978
Alex <alexjameslee@mykolab.com>
2017-08-10 17:53:21 +0100

    moved to gulp and scss, impoved index page, added fixed nav to layout7

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
+// Compile sass files
 gulp.task('sass', function () {
-	  return gulp.src('./assets/sass/**/*.scss')
-	    .pipe(sass.sync().on('error', sass.logError))
-	    .pipe(gulp.dest('./assets/css'));
+    gulp.src('./assets/scss/*')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'));
 });
- 
-gulp.task('sass:watch', function () {
-	  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+
+// The default task (called when you run `gulp`)
+gulp.task('watch', function() {
+  gulp.run('sass');
+
+  // Watch files and run tasks if they change
+
+  gulp.watch(['./assets/scss/*.scss'], function() {
+    gulp.run('sass');
+  })
 });
